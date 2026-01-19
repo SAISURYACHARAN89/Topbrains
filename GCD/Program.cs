06a637c3e7025031e70ca6a063d021536db6e56e
surya charan <psaisuryacharan@gmail.com>
2026-01-19 12:17:51 +0530

    gcd

---
+++ GCD/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static int Euclid(int a,int b,int r)
+    {
+        if(r == 0){return a;}
+        //a = bq + r
+        if(a > b)
+        {
+            r = a % b;
+            return Euclid(b,r,r);
+        }
+        else
+        {
+            r = b % a;
+            return Euclid(a,r,r);
+        }
+    }
+    static void Main()
+    {
+        Console.WriteLine(Euclid(120,25,1));
+
+    }
+}
