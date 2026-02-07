f63c381ab5adb363aced2c37186e5d0536241dd1
surya charan <psaisuryacharan@gmail.com>
2026-02-07 15:47:51 +0530

    luckynumber

---
+++ luckynumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static int DigitSum(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    static bool IsPrime(int n)
+    {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i * i <= n; i += 2)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int m = int.Parse(input[0]);
+        int n = int.Parse(input[1]);
+
+        int count = 0;
+
+        for (int x = m; x <= n; x++)
+        {
+            if (!IsPrime(x))
+            {
+                int sx = DigitSum(x);
+                int sxx = DigitSum(x * x);
+
+                if (sxx == sx * sx)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
